e38fecedb1095a9af4a1778d7e4b59cd9951002e
mat <matteo.gattanini@gmail.com>
2022-10-05 22:52:41 +0200

    a poor man's mouse pan and zoom

--- n-body.cpp
+++ n-body.cpp
@@ -31,9 +31,16 @@ int main()
     // Adding bodies (mass, initial position and speed)
     universe.add_body(10000, {400,400}, {0,0})
             .add_body(100, {200,400}, {0,4})
-            .add_body(200, {700,400}, {0,-2});
+            .add_body(200, {700,400}, {0,-2})
+            .add_body(100, {400,500}, {10,0})
+            .add_body(200, {400,300}, {-5,0});
 
     sf::RenderWindow window(sf::VideoMode(800, 800), "n-body");
+    sf::View view = window.getDefaultView();
+    window.setView(view);
+
+    struct{ sf::Vector2i clicked,center; bool active=false; } pan;
+
 
     sf::Text text;
     sf::Font font;
@@ -77,9 +84,87 @@ int main()
     while( window.isOpen() )
        {
         sf::Event event;
-        while(window.pollEvent(event))
+        while( window.pollEvent(event) )
            {
-            if(event.type == sf::Event::Closed) window.close();
+
+            //switch( event.type )
+            //   {
+            //    case sf::Event::KeyPressed:
+            //        break;
+            //    case sf::Event::Closed:
+            //        window.close();
+            //        break;
+            //   }
+
+
+            if( event.type==sf::Event::Resized )
+               {// Update the view to the new size of the window
+                sf::FloatRect visibleArea(0.f, 0.f, static_cast<float>(event.size.width), static_cast<float>(event.size.height));
+                view.reset(visibleArea);
+                window.setView(view);
+               }
+            else if( event.type==sf::Event::MouseButtonPressed )
+               {
+                if( event.mouseButton.button==sf::Mouse::Left )
+                   {
+                    pan.clicked = {event.mouseButton.x, event.mouseButton.y};
+                    pan.center = window.mapCoordsToPixel(view.getCenter());
+                    pan.active = true;
+                   }
+               }
+            else if( event.type==sf::Event::MouseButtonReleased )
+               {
+                pan.active = false;
+               }
+            else if( event.type==sf::Event::MouseMoved && pan.active )
+               {
+                auto do_pan = [&window, &view, &pan](const sf::Vector2i pixel) -> void
+                   {
+                    //const sf::Vector2f p{ window.mapPixelToCoords(pixel) };
+                    //view.setCenter( sf::Vector2f(pixel) );
+                    //view.setCenter(sf::Vector2f(pan.center + pan.clicked - pixel));
+                    view.move(sf::Vector2f(pan.clicked - pixel));
+                    pan.clicked = pixel;
+                    window.setView(view);
+                   };
+                do_pan({event.mouseMove.x, event.mouseMove.y});
+               }
+            else if( event.type==sf::Event::MouseWheelScrolled )
+               {
+                auto zoom_at = [&window, &view](const sf::Vector2i pixel, const float zoom) -> void
+                   {
+                    const sf::Vector2f c_before{ window.mapPixelToCoords(pixel) };
+                    view.zoom(zoom);
+                    window.setView(view);
+                    const sf::Vector2f c_after{ window.mapPixelToCoords(pixel) };
+                    view.move(c_before - c_after);
+                    window.setView(view);
+                   };
+                const float zoomAmount{ 1.1f }; // 10%
+                zoom_at({ event.mouseWheelScroll.x, event.mouseWheelScroll.y }, event.mouseWheelScroll.delta>0 ? (1.f / zoomAmount) : zoomAmount);
+               }
+            else if( event.type==sf::Event::KeyPressed )
+               {
+                if( event.key.code==sf::Keyboard::Escape )
+                   {
+                    window.close();
+                   }
+               }
+            else if( event.type==sf::Event::Closed )
+               {
+                window.close();
+               }
+
+            // get the current mouse position in the window
+            //sf::Vector2i pixelPos = sf::Mouse::getPosition(window);
+
+            // convert it to world coordinates
+            //sf::Vector2f worldPos = window.mapPixelToCoords(pixelPos);
+            //view.zoom(0.5f);
+            //view.move(100.f, 100.f);
+            //view.setCenter(200.f, 200.f);
+            //    view.zoom(0.5f);
+
            }
 
         const sec duration = clock::now() - before;
